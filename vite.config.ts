bc6a1c90d26a33aa7bde5b4e39fbd7cfc3061c66
Grol-DBT <85458301+Grol-DBT@users.noreply.github.com>
2025-03-21 14:25:15 -0700

    Fix base for development environment

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/map/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'development' ? '/' : '/map/',
   plugins: [react()],
-});
+}));
