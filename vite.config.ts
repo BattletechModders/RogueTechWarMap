c70b924e07eadc925dc53dc22b3ba4b75c8ae19a
wmtorode <wmtorode@gmail.com>
2025-04-12 00:14:39 -0400

    pathing changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig,loadEnv } from ' vite';
 import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : import.meta.env.BASE_URL,
-  plugins: [react()],
-}));
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    base: mode === 'development' ? '/' : env.BASE_URL,
+    plugins: [react()],
+  };
+});
