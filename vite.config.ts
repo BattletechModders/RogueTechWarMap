584892d5e2d11c9eefe389cdd77e1b2a6aad3740
wmtorode <wmtorode@gmail.com>
2025-04-13 01:21:19 -0400

    Test Router change

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: mode === 'development' ? '/' : env.VITE_BASE_URL,
+    base: mode === 'development' ? '/' : '/map',
     plugins: [react()],
   };
 });
