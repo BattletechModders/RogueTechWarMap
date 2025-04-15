230c44befaa8b6125be6da9ffadf223e9676eb62
wmtorode <wmtorode@gmail.com>
2025-04-15 02:21:33 -0400

    fix build error

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    base: mode === 'development' ? '/' : '/map',
+    base: mode === 'development' ? '/' : env.VITE_BASE_URL,
     plugins: [react()],
   };
 });
